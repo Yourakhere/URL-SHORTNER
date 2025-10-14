ed79d4369956835eee80ac50986987bf928890f4
Abhishek kumar <139471330+Yourakhere@users.noreply.github.com>
2025-10-14 22:42:17 +0530

    Update index.js
    Frontend page ur update

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT;
  
 app.use(
   cors({
-    origin: "https://yourakhere-url.vercel.app", 
+    origin: "https://ak-url.vercel.app", 
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
@@ -56,4 +56,5 @@ app.get("/:shortId", async (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`🚀 Server is running on PORT ${PORT}`));
+
 
